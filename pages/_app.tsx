fb33010afe973f7efda8f290b89ba3070efb14fa
MSterpin <92162018+maurosterpin@users.noreply.github.com>
2022-08-05 16:45:53 +0200

    app setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,27 @@ import type { AppProps } from "next/app";
 import { useState, useEffect } from "react";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  const [isSSR, setisSSR] = useState(true);
+
+  useEffect(() => {
+    setisSSR(false);
+  }, []);
+
+  if (isSSR) return null;
+
+  return (
+    <div>
+      Navbar
+      <div className="flex gap-6 md:gap-20">
+        <div className="h-[92vh] overflow-hidden xl:hover:overflow-auto">
+          Sidebar
+        </div>
+        <div className="mt-4 flex flex-col gap-10 overflow-auto h-[88vh] videos flex-1">
+          <Component {...pageProps} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MyApp;
