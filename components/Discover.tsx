504009f55760a0103e571bec4efc181b2f09a06f
MSterpin <92162018+maurosterpin@users.noreply.github.com>
2022-08-08 22:29:26 +0200

    discover style

--- components/Discover.tsx
+++ components/Discover.tsx
@@ -4,7 +4,13 @@ import { useRouter } from "next/router";
 import { topics } from "../utils/constants";
 
 const Discover = () => {
-  return <div>Discover</div>;
+  return (
+    <div className="xl:border-b-2 xl:border-gray-200 pb-6">
+      <p className="text-gray-500 font-semibold m-3 mt-4 hidden xl:block">
+        Discover
+      </p>
+    </div>
+  );
 };
 
 export default Discover;
